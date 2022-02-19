73c7fc3830112e28035e5aef0b6ac127eda5f8cb
́́́ ́́́́́́́́ ́́́́́́́ ́́́́́́ ́́́́́ ́́́́́ ́́́́́́ ́́́́́ <90442760+2r11@users.noreply.github.com>
2022-02-19 19:21:30 +0300

    Update ToolGripper.lua

--- ToolGripper.lua
+++ ToolGripper.lua
@@ -1,7 +1,7 @@
 local char = game.Players.LocalPlayer.Character
 local root = game.Players.LocalPlayer.Character.HumanoidRootPart
 local backpack = game.Players.LocalPlayer.Backpack
-function align(part,x,y,z,xx,yy,zz)
+function align(part, x, y, z, xx, yy, zz)
     local bg = Instance.new("BodyGyro", part)
     local bp = Instance.new("BodyPosition", part)
     bg.MaxTorque = Vector3.new(math.huge, math.huge, math.huge)
@@ -9,39 +9,75 @@ function align(part,x,y,z,xx,yy,zz)
     bg.D = 100
     bp.D = 550
     while true do
-        bp.Position = root.Position + Vector3.new(x,y,z)
-        bg.CFrame = CFrame.Angles(xx,yy,zz) -- math.rad(90)
+        bp.Position = root.Position + Vector3.new(x, y, z)
+        bg.CFrame = CFrame.Angles(xx, yy, zz) -- math.rad(90)
         game:GetService("RunService").RenderStepped:Wait()
     end
 end
 
 function netless()
-    for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
-        if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then 
-            game:GetService("RunService").Stepped:connect(function()
-                v.Velocity = Vector3.new(-30,0,0)
-            end)
-            game:GetService("RunService").Stepped:connect(function()
-                v.Velocity = Vector3.new(45,0,0)
-            end)
-            game:GetService("RunService").RenderStepped:connect(function()
-                v.Velocity = Vector3.new(-30,0,0)
-            end)
-            game:GetService("RunService").RenderStepped:connect(function()
-                v.Velocity = Vector3.new(45,0,0)
-            end)
-            game:GetService("RunService").Heartbeat:connect(function()
-                v.Velocity = Vector3.new(-30,0,0)
-            end)
-            game:GetService("RunService").Heartbeat:connect(function()
-                v.Velocity = Vector3.new(45,0,0)
-            end)
+    if UseOld == true then
+        for i, v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
+            if v:IsA("BasePart") and v.Name ~= "HumanoidRootPart" then
+                game:GetService("RunService").Stepped:connect(
+                    function()
+                        v.Velocity = Vector3.new(-30, 0, 0)
+                    end
+                )
+                game:GetService("RunService").Stepped:connect(
+                    function()
+                        v.Velocity = Vector3.new(45, 0, 0)
+                    end
+                )
+                game:GetService("RunService").RenderStepped:connect(
+                    function()
+                        v.Velocity = Vector3.new(-30, 0, 0)
+                    end
+                )
+                game:GetService("RunService").RenderStepped:connect(
+                    function()
+                        v.Velocity = Vector3.new(45, 0, 0)
+                    end
+                )
+                game:GetService("RunService").Heartbeat:connect(
+                    function()
+                        v.Velocity = Vector3.new(-30, 0, 0)
+                    end
+                )
+                game:GetService("RunService").Heartbeat:connect(
+                    function()
+                        v.Velocity = Vector3.new(45, 0, 0)
+                    end
+                )
+            end
+        end
+    else
+        local origpos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        local pos =
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Position +
+            Vector3.new(math.random(-1500, 1500), 0, math.random(-1500, 1500))
+        local check = true
+        for i, v in pairs(game.Players:GetPlayers()) do
+            if v ~= game.Players.LocalPlayer then
+                if (v.Character.HumanoidRootPart.Position - pos).magnitude <= gethiddenproperty(v, "SimulationRadius") then
+                    check = false
+                end
+            end
+        end
+        if check then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(pos)
+            wait(1)
+            game:GetService("TweenService"):Create(
+                game.Players.LocalPlayer.Character.HumanoidRootPart,
+                TweenInfo.new(1),
+                {CFrame = origpos}
+            ):Play()
         end
     end
 end
 --netless()
-function Grip(x,y,z,xx,yy,zz)
-    for i,v in pairs(backpack:GetChildren()) do
+function Grip(x, y, z, xx, yy, zz)
+    for i, v in pairs(backpack:GetChildren()) do
         if v:IsA("Tool") then
             v.Parent = char
             for i, x in next, v:GetChildren() do
@@ -51,14 +87,14 @@ function Grip(x,y,z,xx,yy,zz)
                 end
             end
             wait(0.3)
-            for i, v in next, char['Right Arm']:GetDescendants() do
+            for i, v in next, char["Right Arm"]:GetDescendants() do
                 v:Destroy()
             end
-            char['Animate']['toolnone']['ToolNoneAnim']['AnimationId'] = 0
+            char["Animate"]["toolnone"]["ToolNoneAnim"]["AnimationId"] = 0
             local part = v:FindFirstChildOfClass("Part")
-            align(part,x,y,z,xx,yy,zz)
-	    wait(0.2)
-	    v.Parent = backpack
+            align(part, x, y, z, xx, yy, zz)
+            wait(0.2)
+            v.Parent = backpack
         end
     end
 end
